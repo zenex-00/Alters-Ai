9ae1957d84bc86bae3871124d07a0c733873c757
Muhammad Zunair Rafique <muhammadzunairrafique654321@gmail.com>
2025-05-17 22:10:09 +0500

    Your commit message here

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const supabase = createClient(
 // Create uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, "Uploads");
 if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(UploadsDir);
+  fs.mkdirSync(uploadsDir);
 }
 
 // Configure multer for file uploads
